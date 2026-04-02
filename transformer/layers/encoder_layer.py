b4629800f0c04bd88d98218a0960f3477d52c87d
Divanshu <97019230+sdivyanshu90@users.noreply.github.com>
2026-04-02 20:15:29 +0530

    Rename import for PositionWiseFeedForward

--- transformer/layers/encoder_layer.py
+++ transformer/layers/encoder_layer.py
@@ -33,8 +33,7 @@ import torch
 import torch.nn as nn
 
 from ..attention.multi_head import MultiHeadAttention
-from .feed_forward import PositionWiseFeedForward
-
+from .feed_forward import PositionWiseFeedForward as FeedForward
 
 class EncoderLayer(nn.Module):
     """
